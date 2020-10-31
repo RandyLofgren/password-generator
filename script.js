dcf55bce8375dd445ac4a5609ee9553c885b345e
RandyLofgren <43276499+RandyLofgren@users.noreply.github.com>
2020-10-31 17:37:34 -0400

    Confirm that user picked at least one type of character working

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"]
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "X", "Y", "Z"]
+var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+var characters = ["!", "@", "#", "$", "%", "^", "&", "*"]
 
 
 // Write password to the #password input
@@ -17,18 +21,25 @@ function writePassword() {
     else {
         writePassword()
     }
-    numberPrompt = prompt("Would you like numbers used in your password? \n Answer 'yes' or 'no'");
-    lowerPrompt = prompt("Would you like lowercase letters used in your password? \n Answer 'yes' or 'no'")
-    upperPrompt = prompt("Would you like uppercase letters used in your password? \n Answer 'yes' or 'no'")
-    specialPrompt = prompt("Would you like special characters used in your password? \n Answer 'yes' or 'no'")
-    if (numberPrompt.toLowerCase() === "yes") {
-        
+    numberPrompt = prompt("Would you like numbers used in your password? \n Answer 'yes' if so.");
+    lowerPrompt = prompt("Would you like lowercase letters used in your password? \n Answer 'yes' if so.")
+    upperPrompt = prompt("Would you like uppercase letters used in your password? \n Answer 'yes' if so.")
+    specialPrompt = prompt("Would you like special characters used in your password? \n Answer 'yes' if so.")
+    if (numberPrompt.toLowerCase() !== "yes") {
+        if (lowerPrompt.toLowerCase() !== "yes") {
+            if (upperPrompt.toLowerCase() !== "yes") {
+                if (specialPrompt.toLowerCase() !== "yes") {
+                    alert("You must pick at least 1 type of character! \n Please Start Over")
+                    writePassword()
+                } else { alert("Thank You!") }
+            } else { alert("Thank You!") }
+        } else { alert("Thank You!") }
 
-    }
-    else {
-        alert("Your password will NOT contain Numbers")
-    }
-  
+
+    } else { alert("Thank You!") }
+
+
+
 
 
     var password = generatePassword();
@@ -47,7 +58,3 @@ generateBtn.addEventListener("click", writePassword);
 
 
 
-var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"]
-var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "X", "Y", "Z"]
-var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
-var characters = ["!", "@", "#", "$", "%", "^", "&", "*"]
